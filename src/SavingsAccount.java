76f29f0b233169e428bb413d3d8e32cd603ee34d
rojeekoju <rojeek2@gmail.com>
2025-01-15 10:25:22 -0800

    added savingacccounts file

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -7,12 +7,28 @@ public class SavingsAccount extends Account {
         this.interestRate = interestRate;
     }
 
-    /*
-    Overload the withdraw method to check if the withdrawal exceeds the overdraft limit.
-    Use assertions.
-    Overload the deposit method to check if the deposit is greater than 0.
-    Use assertions.
-     */
+    
+    //Overload the withdraw method to check if the withdrawal exceeds the overdraft limit.
+    //Use assertions.
+
+    public void withdraw(double amount) throws InsufficientFundsException {
+        assert amount > 0 : "Withdrawal amount must be positive";
+
+        if (amount > balance) {
+            throw new InsufficientFundsException("Insufficient funds for the withdrawal.");
+        }
+
+        balance -= amount;
+    }
+
+
+
+    //Overload the deposit method to check if the deposit is greater than 0.
+    //Use assertions.
+    public void deposit(double amount) {
+        assert amount > 0 : "Deposit amount must be positive";
+        balance += amount;
+    }
 
     public double getInterestRate() {
         return interestRate;
