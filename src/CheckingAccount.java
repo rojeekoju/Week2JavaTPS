f619ddc33eee45b74200a2f013041e7db7d9c1d8
Edale Miguel <edale.miguel@seattlecolleges.edu>
2025-01-15 09:24:12 -0800

    Updated CheckingAccount

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -7,12 +7,24 @@ public class CheckingAccount extends Account {
         this.overdraftLimit = overdraftLimit;
     }
 
-    /*
-    Overload the withdraw method to check if the withdrawal exceeds the overdraft limit.
-    Use assertions.
-    Overload the deposit method to check if the deposit is greater than 0.
-    Use assertions.
+    /**
+     *     Overload the withdraw method to check if the withdrawal exceeds the overdraft limit.
+     *     Use assertions.
      */
+    public void withdraw(double amount) {
+        assert amount > 0: "Amount must be greater than zero";
+        assert (amount - getBalance() <= overdraftLimit)
+                : "Withdrawal not allowed. Exceeds the overdraft limit.";
+        balance -= amount;
+    }
+    /**
+     *     Overload the deposit method to check if the deposit is greater than 0.
+     *     Use assertions.
+     */
+    public void deposit (double amount) {
+        assert amount > 0: "Amount must be greater than zero";
+        balance += amount;
+    }
 
     public double getOverdraftLimit() {
         return overdraftLimit;
